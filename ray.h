1f32e661f380a30f6c14c6f51cb419528410bee9
George George <srdrmrdave@gmail.com>
2020-11-26 14:30:15 +1000

    initial

---
+++ ray.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "vec3.h"
+
+struct ray {
+	point3 orig;
+	vec3 dir;
+	
+	ray() {}
+	ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+
+	point3 origin() const {return orig;}
+	vec3 direction() const {return dir;}
+};
